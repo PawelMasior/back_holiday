154d6cb2e7caaa9e6398eb50e45549e5524ac0d1
pablomasior <pawel.masior@lotangroup.com>
2024-09-17 16:13:47 +0200

    report hide

--- main.py
+++ main.py
@@ -99,114 +99,114 @@ app.add_middleware(
 @app.get("/")
 def home(): return 'holiday'
             
-@app.get("/report/")#, tags=["APIs"], response_model=str)
-def _report(
-        name_city: str = Query("Napoli"),
-        desc_user: str = Query("local specialities,low budget,adventure"),
-        date_fr: str = Query("07-12-2024"),
-        date_to: str = Query("14-12-2024"),
-        bool_eating: bool = Query(False),
-        bool_events: bool = Query(False),
-        # bool_museums: bool = Query(True),
-        # bool_local: bool = Query(True),
-        # bool_avoid: bool = Query(True),
-        ):
-    # =============================================================================
-    query = {
-        'name_city': name_city,
-        'desc_user': desc_user,
-        'date_fr': date_fr,
-        'date_to': date_to,
-        }
-    scope = {
-        'eating': bool_eating,
-        'events': bool_events,
-        'museums': False,
-        'safety': False,
-        'health': False,
-        'shopping': False,
-        'around': False,
-        'accomodation': False,
-        'attractions': False,
-        'transportation': False,
-        'nightlife': False,
-        'wellness': False,
-        }
-    # =============================================================================
-    autogen.register_function(web_search, caller=agent_researcher, executor=executor,
-        name="web_search",
-        description="""Searches internet with query, providing concise or detailed content as needed."""
-    )
-    autogen.register_function(web_page, caller=agent_researcher, executor=executor,
-        name="web_page",
-        description="""Retrieves website content by scraping URL."""
-    )
-    autogen.register_function(save_report, caller=agent_researcher, executor=executor,
-        name="save_report",
-        description="""Saves report in markdown format."""
-    )
+# @app.get("/report/")#, tags=["APIs"], response_model=str)
+# def _report(
+#         name_city: str = Query("Napoli"),
+#         desc_user: str = Query("local specialities,low budget,adventure"),
+#         date_fr: str = Query("07-12-2024"),
+#         date_to: str = Query("14-12-2024"),
+#         bool_eating: bool = Query(False),
+#         bool_events: bool = Query(False),
+#         # bool_museums: bool = Query(True),
+#         # bool_local: bool = Query(True),
+#         # bool_avoid: bool = Query(True),
+#         ):
+#     # =============================================================================
+#     query = {
+#         'name_city': name_city,
+#         'desc_user': desc_user,
+#         'date_fr': date_fr,
+#         'date_to': date_to,
+#         }
+#     scope = {
+#         'eating': bool_eating,
+#         'events': bool_events,
+#         'museums': False,
+#         'safety': False,
+#         'health': False,
+#         'shopping': False,
+#         'around': False,
+#         'accomodation': False,
+#         'attractions': False,
+#         'transportation': False,
+#         'nightlife': False,
+#         'wellness': False,
+#         }
+#     # =============================================================================
+#     autogen.register_function(web_search, caller=agent_researcher, executor=executor,
+#         name="web_search",
+#         description="""Searches internet with query, providing concise or detailed content as needed."""
+#     )
+#     autogen.register_function(web_page, caller=agent_researcher, executor=executor,
+#         name="web_page",
+#         description="""Retrieves website content by scraping URL."""
+#     )
+#     autogen.register_function(save_report, caller=agent_researcher, executor=executor,
+#         name="save_report",
+#         description="""Saves report in markdown format."""
+#     )
 
-    autogen.register_function(get_info, caller=agent_secretary, executor=executor,
-        name="get_info",
-        description="""Get all collected informations on destination place."""
-    )
-    autogen.register_function(save_report_final, caller=agent_secretary, executor=executor,
-        name="save_report_final",
-        description="""Prepare aggregated report for the user."""
-    )
-    # =============================================================================
-    queue_researcher = [
-        {"recipient": agent_researcher, "sender": executor, "summary_method": "last_msg"},
-        {"recipient": agent_planner, "sender": agent_researcher, "summary_method": "reflection_with_llm"},
-        ]
-    agent_researcher.register_nested_chats(trigger=agent_planner, chat_queue=queue_researcher)
+#     autogen.register_function(get_info, caller=agent_secretary, executor=executor,
+#         name="get_info",
+#         description="""Get all collected informations on destination place."""
+#     )
+#     autogen.register_function(save_report_final, caller=agent_secretary, executor=executor,
+#         name="save_report_final",
+#         description="""Prepare aggregated report for the user."""
+#     )
+#     # =============================================================================
+#     queue_researcher = [
+#         {"recipient": agent_researcher, "sender": executor, "summary_method": "last_msg"},
+#         {"recipient": agent_planner, "sender": agent_researcher, "summary_method": "reflection_with_llm"},
+#         ]
+#     agent_researcher.register_nested_chats(trigger=agent_planner, chat_queue=queue_researcher)
 
-    queue_secretary = [
-        {"recipient": agent_secretary, "sender": executor, "summary_method": "last_msg"},
-        {"recipient": agent_planner, "sender": agent_secretary, "summary_method": "reflection_with_llm"},
-        ]
-    agent_secretary.register_nested_chats(trigger=agent_planner, chat_queue=queue_secretary)
-    # =============================================================================
-    Plan = []
-    Plan = [prompt_start(query)]
-    if scope['eating']: Plan += [prompt_eating(query)]
-    if scope['events']: Plan += [prompt_events(query)]
-    # =============================================================================
-    Conv = [{
-                "recipient": agent_researcher,
-                "message": f"""
-                {t}
+#     queue_secretary = [
+#         {"recipient": agent_secretary, "sender": executor, "summary_method": "last_msg"},
+#         {"recipient": agent_planner, "sender": agent_secretary, "summary_method": "reflection_with_llm"},
+#         ]
+#     agent_secretary.register_nested_chats(trigger=agent_planner, chat_queue=queue_secretary)
+#     # =============================================================================
+#     Plan = []
+#     Plan = [prompt_start(query)]
+#     if scope['eating']: Plan += [prompt_eating(query)]
+#     if scope['events']: Plan += [prompt_events(query)]
+#     # =============================================================================
+#     Conv = [{
+#                 "recipient": agent_researcher,
+#                 "message": f"""
+#                 {t}
                 
-                Focuse on the task and perform only necessary steps.                
-                Finalize task with saving collected information into the report!
-                """,
-                "max_turns": 10,
-                "max_round": 30,
-                "summary_method": "reflection_with_llm",
-            } for t in Plan]
-    Conv += [{
-                "recipient": agent_secretary,
-                "message": f"""
-                1. Get all collected informations on destination place.
-                2. {prompt_final(query, scope)} 
+#                 Focuse on the task and perform only necessary steps.                
+#                 Finalize task with saving collected information into the report!
+#                 """,
+#                 "max_turns": 10,
+#                 "max_round": 30,
+#                 "summary_method": "reflection_with_llm",
+#             } for t in Plan]
+#     Conv += [{
+#                 "recipient": agent_secretary,
+#                 "message": f"""
+#                 1. Get all collected informations on destination place.
+#                 2. {prompt_final(query, scope)} 
                 
-                Focuse on the task and perform only necessary steps.                
-                Finalize task with saving collected information into the report! 
-                """,
-                "max_turns": 10,
-                "max_round": 30,
-                "summary_method": "reflection_with_llm",
-            }]
-    # =============================================================================
-    logging_session_id = autogen.runtime_logging.start(config={"dbname": dbname})
-    chat_results = agent_planner.initiate_chats(Conv)
-    autogen.runtime_logging.stop()
-    # =============================================================================
+#                 Focuse on the task and perform only necessary steps.                
+#                 Finalize task with saving collected information into the report! 
+#                 """,
+#                 "max_turns": 10,
+#                 "max_round": 30,
+#                 "summary_method": "reflection_with_llm",
+#             }]
+#     # =============================================================================
+#     logging_session_id = autogen.runtime_logging.start(config={"dbname": dbname})
+#     chat_results = agent_planner.initiate_chats(Conv)
+#     autogen.runtime_logging.stop()
+#     # =============================================================================
 
-    # with open(os.path.join('temporary', 'report_final.md'), 'w', encoding='utf-8') as f:
-    #     f.write(report_final)
+#     # with open(os.path.join('temporary', 'report_final.md'), 'w', encoding='utf-8') as f:
+#     #     f.write(report_final)
 
-    return HTMLResponse(content=report_final)
+#     return HTMLResponse(content=report_final)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
